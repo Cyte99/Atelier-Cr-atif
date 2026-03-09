16fc2604c730a42298422b42fec6975c6aca0ee6
Cyte99 <chengkai.chen@student.hepl.be>
2026-03-09 09:03:53 +0100

    ajout des scripts dans les boutons

--- Atelier/Assets/Script/PauseMenu.cs
+++ Atelier/Assets/Script/PauseMenu.cs
@@ -1,16 +1,36 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] GameObject pauseMenu;
+
+    public void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+    private void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Menu()
     {
-        
+        SceneManager.LoadScene(0);
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
