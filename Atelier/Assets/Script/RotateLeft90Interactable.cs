f8c5f56fc1d64057c77663505e63c861ccf1d819
Simon Moes <simon.moes@student.hepl.be>
2026-03-19 20:52:18 +0100

    Son des statue qui tournent

--- Atelier/Assets/Script/RotateLeft90Interactable.cs
+++ Atelier/Assets/Script/RotateLeft90Interactable.cs
@@ -6,11 +6,25 @@ public class RotateLeft90Interactable : MonoBehaviour, IInteractable
     [SerializeField] private float turnDegrees = 90f;
     [SerializeField] private PuzzleManager puzzleManager;
 
+    [Header("Son")]
+    [SerializeField] private AudioClip rotateSound;
+    [SerializeField][Range(0f, 1f)] private float volume = 1f;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+            audioSource = gameObject.AddComponent<AudioSource>();
+    }
+
     public void Interact(Player_Mouvement interactor)
     {
         transform.Rotate(0f, -turnDegrees, 0f, Space.World);
 
-        //re-check after rotating
+        audioSource.PlayOneShot(rotateSound, volume);
+
         if (puzzleManager != null)
         {
             puzzleManager.CheckPuzzle();
